1f90bbfd8cf93d0c05183e25cdc2a52f1c9c5533
interdimensionalpizza <michaellake05@gmail.com>
2023-06-07 14:06:10 -0700

    finished challenge

--- regexp.js
+++ regexp.js
@@ -1,13 +1,18 @@
 // takes in a string as input and should return true if valid
 function validateEmail(email) {
-    // TO DO
-    return true;
+    regex = /^[^@\s]+@[^@\s]+\.[^@\s]+/g;
+    if (email.match(regex)) {
+        return true;
+    }
 }
 
-// takes in a string as input and returns true if valud
+// takes in a string as input and returns true if value
 function validateUrls(url) {
-    // TO DO
-    return true;
+    regex = /^https?:\/\/www\.[a-z]+\.(org|com|net)/g;
+    if (url.match(regex)) {
+        console.log(url.match(regex))
+        return true;
+    }
 }
 
 module.exports = {
